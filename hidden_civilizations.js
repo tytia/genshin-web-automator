9cc065e79be4838a366680cdc22eaa870db2fdeb
Terry Tian <terrytian2004+git@gmail.com>
2022-10-20 02:24:02 +1100

    Implemented pop-up handling in main script

--- hidden_civilizations.js
+++ hidden_civilizations.js
@@ -3,48 +3,85 @@
  * Event period: September 29, 2022 - October 31, 2022
  * Author: Terry Tian
  * 
- * Automates the redemption of the 'x60 Primogems' reward, redeeming it on cooldown ASAP before it runs out.
+ * Automates the redemption of rewards, redeeming them on cooldown ASAP before it runs out.
  * 
  * INSTRUCTIONS FOR USE:
  * Press Ctrl+Shift+I on the event webpage and paste into the DevTools console to run.
  */
 
-const coinsNode = document.querySelector(".hyl-task-count_text");
 const redeemBtn = document.querySelector(".right-bottom_right.daily"); // 'x60 Primogems' reward redeem button
 const timerNode = document.querySelector(".van-count-down"); // timer display
+const coinsNode = document.querySelector(".hyl-task-count_text");
 var coins = coinsNode.textContent;
+var popUp, cost, redeemCountNode, start, i;
 
 const btnObserver = new MutationObserver(redeemPrimos);
-const popUpObserver = new MutationObserver(redeemPopUp);
 const coinsObserver = new MutationObserver(updateCoins);
-const config1 = {characterData: true};
-const config2 = {childList: true};
+const popUpObserver = new MutationObserver(popUpHandler);
+const popUpObserver2 = new MutationObserver(redeemPopUp);
+const popUpObserver3 = new MutationObserver(redeemPopUp2);
 
-btnObserver.observe(timerNode.childNodes[0], config1);
-coinsObserver.observe(coinsNode.childNodes[0], config1)
+btnObserver.observe(timerNode.childNodes[0], {characterData: true});
+coinsObserver.observe(coinsNode.childNodes[0], {characterData: true});
 
+// if script is activated when reward is already available for redemption
 if (timerNode.textContent === "Redeem Now") {
-    popUpObserver.observe(document.body, config2);
+    start = performance.now();
+    btnObserver.disconnect();
+    popUpObserver.observe(document.body, {childList: true});
     redeemBtn.click();
 }
 
 function redeemPrimos() {
     if (timerNode.textContent === "Redeem Now") {
-        popUpObserver.observe(document.body, config2);
+        start = performance.now();
+        btnObserver.disconnect();
+        popUpObserver.observe(document.body, {childList: true});
         redeemBtn.click();
     }
 }
 
-function redeemPopUp(mutations) {
-    console.log(mutations);
-    for (const mutation of mutations) {
-        if (mutation.type === "childList") {
-            console.log(mutation.addedNodes);
+function popUpHandler(mutations) {
+    if (mutations[0].addedNodes.length) {
+        popUpObserver.disconnect();
+        popUp = mutations[0].addedNodes[0];
+        popUpObserver2.observe(popUp, {attributes: true});
+    }
+}
+
+function redeemPopUp() {
+    var confirmBtn = popUp.querySelector(".draw-btn");
+    var redeemAdd, redeemMax;
+    
+    if (confirmBtn !== null) {
+        popUpObserver2.disconnect();
+        redeemAdd = popUp.querySelector(".operate-btn:not(operate-btn_disabled)");
+        cost = popUp.querySelector(".award-num").textContent;
+        
+        if (redeemAdd === null) {
+            confirmBtn.click();
+            console.log(`Execution time: ${(performance.now() - start).toFixed(2)}ms`);
+        }
+        else {
+            redeemCountNode = popUp.querySelector(".number-input");
+            redeemMax = Math.floor(coins / cost);
+            popUpObserver3.observe(redeemCountNode.childNodes[0], {characterData: true});
+            redeemAdd.click();
+            i++;
         }
     }
+}
 
-    btnObserver.disconnect();
-    popUpObserver.disconnect();
+function redeemPopUp2() {
+    if (i == redeemMax) {
+        popUpObserver3.disconnect();
+        confirmBtn.click();
+        console.log(`Execution time: ${(performance.now() - start).toFixed(2)}ms`);
+    }
+    else {
+        redeemAdd.click();
+        i++;
+    }
 }
 
 function updateCoins() {
--- popup_test.js
+++ popup_test.js
@@ -12,7 +12,7 @@ function callback(mutations) {
     if (mutations[0].addedNodes.length) {
         popUp = mutations[0].addedNodes[0];
         observer.disconnect();
-        observer2.observe(popUp, {childList: true, subtree: true, attributes: true});
+        observer2.observe(popUp, {attributes: true});
     }
 }
 
@@ -20,14 +20,14 @@ function callback2(mutations) {
     console.log(mutations);
     var tab = popUp.querySelector(".tab-item:not(.tab-item-actived)");
     if (tab != null) {
-        observer3.observe(tab, {attributes: true})
-        tab.click();
         observer2.disconnect();
+        observer3.observe(tab, {attributes: true});
+        tab.click();
     }
 }
 
 function callback3() {
     popUp.querySelector(".tab-item:not(.tab-item-actived)").click();
-    console.log(`Execution time: ${(performance.now() - start).toFixed(2)}ms`)
+    console.log(`Execution time: ${(performance.now() - start).toFixed(2)}ms`);
     observer3.disconnect();
 }
